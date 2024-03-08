abd3674b735a4cc2aa260cc090953d6d5d316825
Bruno Raniere <brunoraniere2@gmail.com>
2024-03-08 15:25:44 -0300

    Add slide and get data from sign

--- src/scripts/sign.js
+++ src/scripts/sign.js
@@ -4,4 +4,41 @@ $(document).ready(function() {
         var sanitizedValue = inputValue.replace(/\s/g, '');
         $(this).val(sanitizedValue);
     });
+
+    $('.sign__slide').slick({
+        prevArrow: false,
+        nextArrow: false,
+        swipe: false
+    });
+
+    $('.sign__slide--swipe').on('click', function() {
+        $('.sign__slide').slick('slickNext');
+    });
+
+    $('.button--sign').on('click', function() {
+        const father = $(this).parent().parent();
+        const fatherH1Value = father.find('h1').text().trim().replace(/\s+/g, '');
+
+        const username = $(this).parent().find('.input--username').val();
+        const password = $(this).parent().find('.input--password').val();
+
+        let jsonUserData = {
+            'username': username,
+            'password': password
+        }
+
+        switch (fatherH1Value) {
+            case 'Sign-in':
+                // enviar json ao servidor com axios
+                break;
+
+            case 'Sign-up':
+                const confirmPassword = $(this).parent().find('.input--confirm-password').val();
+
+                jsonUserData['confirmPassword'] = confirmPassword;
+                break;
+        }
+
+        console.log(jsonUserData);
+    });
 });
